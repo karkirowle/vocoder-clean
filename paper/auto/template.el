8a7f8ca6022aeb31c51419624e4fda88c0b41907
booomkin <szerk.animeblog@gmail.com>
2019-02-19 16:18:21 +0100

    Paper stuff

---
+++ paper/auto/template.el
@@ -0,0 +1,35 @@
+(TeX-add-style-hook
+ "template"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "init_pos"
+    "retraining"
+    "retraining_linear"
+    "article"
+    "art10"
+    "INTERSPEECH2018"
+    "tikz"
+    "url"
+    "blindtext")
+   (LaTeX-add-labels
+    "fig1"
+    "tab:electrodes"
+    "tab:example"
+    "nnexperiment"
+    "eq1"
+    "eq2"
+    "eq3"
+    "eq4"
+    "learning_curve"
+    "retraining_linear")
+   (LaTeX-add-bibliographies
+    "paper1"))
+ :latex)
+
