4582e6f364c462d6ecad4350f9e39ce5cbc909cc
booomkin <szerk.animeblog@gmail.com>
2019-03-21 10:19:13 +0100

    Paper update

--- paper/auto/template.el
+++ paper/auto/template.el
@@ -3,15 +3,15 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "a4paper")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "init_pos"
     "blstm_act"
-    "retraining"
+    "retraining_lc"
     "article"
     "art10"
     "INTERSPEECH2018"
@@ -29,9 +29,9 @@
     "tab:all_data"
     "tab:architectures"
     "section:speech"
-    "learning_curve"
     "section:visualisation"
-    "section:limitations")
+    "section:limitations"
+    "learning_curve")
    (LaTeX-add-bibliographies
     "paper1"))
  :latex)
